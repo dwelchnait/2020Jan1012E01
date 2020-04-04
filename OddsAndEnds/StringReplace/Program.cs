7ee903f27fada8f868c75540de987f2088621c3c
dwelchnait <dwelch@nait.ca>
2020-04-03 19:36:48 -0600

    odds and ends solution
    string methods and streamwriter

---
+++ OddsAndEnds/StringReplace/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReplace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //.Replace(char, char) string method
+            string myString = "CPSC1012 Fundmental Programing";
+            string inputString = "";
+            char oldchar;
+            char newchar;
+            string oldstring = "";
+            string newstring = "";
+            do
+            {
+                inputString = GetString("Get character/string you wish to replace");
+                
+                if (!inputString.Equals("-1")) //test to see if i quit
+                {
+                    Console.WriteLine($"your old string is {myString}");
+                    oldstring = inputString;
+                    inputString = GetString("Get new character/string you wish to use in replacement");
+                    newstring = inputString;
+                    //.Replace() will replace any occurance of the old character with the replacement char
+                    //.Replace() returns its work as a string
+                    //you need to receive the string on an assignment statement
+                    myString = myString.Replace(oldstring, newstring);
+                    Console.WriteLine($"your new string is {myString}");
+                }
+            } while (!inputString.Equals("-1"));
+            Console.ReadKey();
+        }
+
+        static string GetString(string prompt)
+
+        {
+            Console.Write($"{prompt}\t");
+            string input = Console.ReadLine();
+            return input;
+        }
+    }
+}
---
+++ OddsAndEnds/WriteCSV/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //need the using System.IO
+            //need location a) absolute path b) find your special directory c) FileOpenDialog
+            //StreamWriter to handle the outgoing data
+            
+            //by default files are replaced
+            //you can add a parameter to the StreamWriter that indicates appending or not
+            //if the file does not exist it will be created
+            StreamWriter writer = new StreamWriter($"c:\\work\\outputfile.txt", append: false);
+            writer.WriteLine("don welch, CPSC1012, 98");
+            //writer.Close();
+            //writer = new StreamWriter($"c:\\work\\outputfile.txt", append: true);
+            writer.WriteLine("shirely ujest, CPSC1012, 49");
+           // writer.Close();
+           // writer = new StreamWriter($"c:\\work\\outputfile.txt", append: true);
+            writer.WriteLine("lowand behold, CPSC1012, 78");
+            writer.Close();
+        }
+    }
+}
